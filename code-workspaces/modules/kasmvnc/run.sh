8358a9a6ef5bd982448c110ebbfd8304adb7a81b
fabaindaiz <fabiandiaz@ug.uchile.cl>
2023-11-03 22:44:35 -0300

    fix: kasmvncserver script

--- code-workspaces/modules/kasmvnc/run.sh
+++ code-workspaces/modules/kasmvnc/run.sh
@@ -7,7 +7,7 @@ RESET='\033[0m'
 printf "$${BOLD}Installing kasmvnc!\n"
 
 sudo apt install -y libgbm1 libgl1 libxcursor1 libxfixes3 libxfont2 libxrandr2 libxshmfence1 libxtst6 ssl-cert xauth x11-xkb-utils xkb-data libswitch-perl libyaml-tiny-perl libhash-merge-simple-perl liblist-moreutils-perl libtry-tiny-perl libdatetime-timezone-perl >/dev/null
-sudo apt install -y dbus-x11 xvfb xfwm4 libupower-glib3 upower xfce4 xfce4-goodies xfce4-terminal xfce4-panel xfce4-session chromium >/dev/null
+sudo apt install -y dbus-x11 xvfb xfwm4 libupower-glib3 upower xfce4 xfce4-goodies xfce4-terminal xfce4-panel xfce4-session >/dev/null
 sudo apt remove -y xfce4-battery-plugin xfce4-power-manager-plugins xfce4-pulseaudio-plugin light-locker >/dev/null
 
 output=$(sudo curl -L "https://github.com/kasmtech/KasmVNC/releases/download/v1.2.0/kasmvncserver_bookworm_1.2.0_amd64.deb" --output /tmp/kasm.deb && sudo dpkg -i /tmp/kasm.deb && sudo apt-get install -f -y)
@@ -21,6 +21,8 @@ KASMVNC_SERVER="kasmvncserver"
 
 # Configure kasmvnc
 vncpasswd -u coder -rwn
+sudo adduser coder ssl-cert
+sudo apt install -y chromium >/dev/null
 
 echo "👷 Running $KASMVNC_SERVER -disableBasicAuth -select-de xfce in the background..."
 echo "Check logs at ${LOG_PATH}!"
