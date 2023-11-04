2be9632f3dba5345a03f3f6c69c5e0c9a5a7e68d
fabaindaiz <fabiandiaz@ug.uchile.cl>
2023-11-03 21:59:14 -0300

    fix: kasmvncserver script

--- code-workspaces/modules/kasmvnc/run.sh
+++ code-workspaces/modules/kasmvnc/run.sh
@@ -9,7 +9,7 @@ printf "$${BOLD}Installing kasmvnc!\n"
 sudo apt install -y libgbm1 libgl1 libxcursor1 libxfixes3 libxfont2 libxrandr2 libxshmfence1 libxtst6 ssl-cert xauth x11-xkb-utils xkb-data libswitch-perl libyaml-tiny-perl libhash-merge-simple-perl liblist-moreutils-perl libtry-tiny-perl libdatetime-timezone-perl >/dev/null
 sudo apt install -y dbus-x11 xvfb xfwm4 libupower-glib3 upower xfce4 xfce4-goodies xfce4-terminal xfce4-panel xfce4-session >/dev/null
 
-sudo apt remove -y xfce4-battery-plugin xfce4-power-manager-plugins xfce4-pulseaudio-plugin light-locker
+sudo apt remove -y xfce4-battery-plugin xfce4-power-manager-plugins xfce4-pulseaudio-plugin light-locker >/dev/null
 
 output=$(sudo curl -L "https://github.com/kasmtech/KasmVNC/releases/download/v1.2.0/kasmvncserver_bookworm_1.2.0_amd64.deb" --output /tmp/kasm.deb && sudo dpkg -i /tmp/kasm.deb && sudo apt-get install -f -y)
 if [ $? -ne 0 ]; then
@@ -20,9 +20,8 @@ printf "🥳 kasmvnc has been installed.\n\n"
 
 KASMVNC_SERVER="kasmvncserver"
 
-# Initialize the Xvfb display
-#sudo sed -i 's/^allowed_users=.*/allowed_users=anybody/' /etc/X11/Xwrapper.config
-echo "coder:$5$root$5g9u2JOfNYQBraXBdyKxTAxOOLiDy7f0l3ORZsF4Dx/:w" | sudo tee -a /root/.kasmpasswd
+# Create coder user for kasmvnc
+echo "coder:$5$root$5g9u2JOfNYQBraXBdyKxTAxOOLiDy7f0l3ORZsF4Dx/:w" | sudo tee -a /root/.kasmpasswd >/dev/null
 sudo vncpasswd -u coder -rwn
 
 echo "👷 Running $KASMVNC_SERVER -disableBasicAuth -select-de xfce in the background..."
